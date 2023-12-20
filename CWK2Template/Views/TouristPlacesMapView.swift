eb058b0b471852c460ea7c4034a74f03ee18742d
Rumeth Randombage <kumudanarumeth@gmail.com>
2023-12-20 14:03:39 +0530

    Initial Commit

---
+++ CWK2Template/Views/TouristPlacesMapView.swift
@@ -0,0 +1,63 @@
+//
+//  TouristPlacesMapView.swift
+//  CWK2Template
+//
+//  Created by girish lukka on 29/10/2023.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+import MapKit
+
+struct TouristPlacesMapView: View {
+    @EnvironmentObject var weatherMapViewModel: WeatherMapViewModel
+    @State var locations: [Location] = []
+    @State var  mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5216871, longitude: -0.1391574), latitudinalMeters: 600, longitudinalMeters: 600)
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 5) {
+                if weatherMapViewModel.coordinates != nil {
+                    VStack(spacing: 10){
+                        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                            MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.coordinates.latitude, longitude: location.coordinates.longitude))
+                            }
+                        }
+                            .frame(height: 300)
+                            .ignoresSafeArea()
+                        VStack{
+                            Text("This is a locally defined map for starter template")
+                            Text("A map of the user-entered location should be shown here")
+                            Text("Map should also show pins of tourist places")
+                        }.multilineTextAlignment(.leading)
+                            .lineLimit(nil)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                }
+                List{
+                    HStack{
+                        VStack {
+                            Text("Tourist place Image")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Text("Tourist place details")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Text("See images in coursework spec")
+                        }
+                    }
+                }
+            }.frame(height:700)
+            .onAppear {
+                locations = weatherMapViewModel.loadLocationsFromJSONFile()
+            }
+        }
+    }
+
+
+
+struct TouristPlacesMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        TouristPlacesMapView()
+    }
+}
