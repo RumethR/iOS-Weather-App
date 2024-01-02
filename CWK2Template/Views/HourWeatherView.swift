c373a845f8dc67cf8256a539462ff7fadf0c3974
Rumeth Randombage <kumudanarumeth@gmail.com>
2024-01-02 07:39:02 +0530

    Added Hourly Forecast to second tab without breaking anything

--- CWK2Template/Views/HourWeatherView.swift
+++ CWK2Template/Views/HourWeatherView.swift
@@ -12,7 +12,9 @@ struct HourWeatherView: View {
 
     var body: some View {
         let formattedDate = DateFormatterUtils.formattedDateWithDay(from: TimeInterval(current.dt))
-        VStack(alignment: .leading, spacing: 5) {
+        let hourlyForecast = current.weather.first?.weatherDescription.rawValue.capitalized ?? "N/A"
+        
+        VStack(spacing: 5) {
             Text(formattedDate)
                 .font(.body)
 
@@ -20,14 +22,17 @@ struct HourWeatherView: View {
                 .background(Color.white)
                 .foregroundColor(.black)
 
-            Text("Add style & other elements")
-                .frame(width: 125)
-                .font(.body)
-                .multilineTextAlignment(.leading)
-                .lineLimit(nil) 
-                .padding(.horizontal)
-                .background(Color.white)
-                .foregroundColor(.black)
+            Text("\((Double)(current.temp), specifier: "%.0f") ºC") //Converting using Int() will round down the value
+                 .font(.body)
+                 .padding(.horizontal)
+                 .background(Color.white)
+                 .foregroundColor(.black)
+
+             Text(hourlyForecast)
+                 .font(.body)
+                 .padding(.horizontal)
+                 .background(Color.white)
+                 .foregroundColor(.black)
         }
         
     }
